1dff360b011ddbb80a5adf44d7991531fe63d8b9
Sharan Duggirala <sd12832@my.bristol.ac.uk>
2017-11-18 18:40:41 -0800

    Made the code modular - discovered consensus is not being used

---
+++ coffee_functions.py
@@ -0,0 +1,44 @@
+from flask import request
+import json
+
+def find_new_chains(peer_nodes):
+  # Get the blockchains of every
+  # other node
+  other_chains = []
+  for node_url in peer_nodes:
+    # Get their chains using a GET request
+    block = requests.get(node_url + "/blocks").content
+    # Convert the JSON object to a Python dictionary
+    block = json.loads(block)
+    # Add it to our list
+    other_chains.append(block)
+  return other_chains
+
+def consensus(blockchain, peer_nodes):
+  # Get the blocks from other nodes
+  other_chains = find_new_chains(peer_nodes)
+  # If our chain isn't longest,
+  # then we store the longest chain
+  longest_chain = blockchain
+  for chain in other_chains:
+    if len(longest_chain) < len(chain):
+      longest_chain = chain
+  # If the longest chain isn't ours,
+  # then we stop mining and set
+  # our chain to the longest one
+  blockchain = longest_chain
+
+def proof_of_work(last_proof):
+  # Create a variable that we will use to find
+  # our next proof of work
+  incrementor = last_proof + 1
+  # Keep incrementing the incrementor until
+  # it's equal to a number divisible by 9
+  # and the proof of work of the previous
+  # block in the chain
+  while not (incrementor % 9 == 0 and incrementor % last_proof == 0):
+    incrementor += 1
+  # Once that number is found,
+  # we can return it as a proof
+  # of our work
+  return incrementor
--- coffeecoin-server.py
+++ coffeecoin-server.py
@@ -1,32 +1,9 @@
 from flask import Flask
 from flask import request
 import json
-import hashlib as hasher
-import datetime as date
 node = Flask(__name__)
-
-# Define what a Coffeecoin block is
-class Block:
-  def __init__(self, index, timestamp, data, previous_hash):
-    self.index = index
-    self.timestamp = timestamp
-    self.data = data
-    self.previous_hash = previous_hash
-    self.hash = self.hash_block()
-  
-  def hash_block(self):
-    sha = hasher.sha256()
-    sha.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash))
-    return sha.hexdigest()
-
-# Generate genesis block
-def create_genesis_block():
-  # Manually construct a block with
-  # index zero and arbitrary previous hash
-  return Block(0, date.datetime.now(), {
-    "proof-of-work": 9,
-    "transactions": None
-  }, "0")
+from Block import *
+from coffee_functions import *
 
 # A completely random address of the owner of this node
 miner_address = "q3nf394hjg-random-miner-address-34nf3i4nflkn3oi"
@@ -103,48 +80,6 @@ def print_blocks():
     else:
       blocklist =  blocklist + assembled
   return blocklist
-
-def find_new_chains():
-  # Get the blockchains of every
-  # other node
-  other_chains = []
-  for node_url in peer_nodes:
-    # Get their chains using a GET request
-    block = requests.get(node_url + "/blocks").content
-    # Convert the JSON object to a Python dictionary
-    block = json.loads(block)
-    # Add it to our list
-    other_chains.append(block)
-  return other_chains
-
-def consensus():
-  # Get the blocks from other nodes
-  other_chains = find_new_chains()
-  # If our chain isn't longest,
-  # then we store the longest chain
-  longest_chain = blockchain
-  for chain in other_chains:
-    if len(longest_chain) < len(chain):
-      longest_chain = chain
-  # If the longest chain isn't ours,
-  # then we stop mining and set
-  # our chain to the longest one
-  blockchain = longest_chain
-
-def proof_of_work(last_proof):
-  # Create a variable that we will use to find
-  # our next proof of work
-  incrementor = last_proof + 1
-  # Keep incrementing the incrementor until
-  # it's equal to a number divisible by 9
-  # and the proof of work of the previous
-  # block in the chain
-  while not (incrementor % 9 == 0 and incrementor % last_proof == 0):
-    incrementor += 1
-  # Once that number is found,
-  # we can return it as a proof
-  # of our work
-  return incrementor
 
 @node.route('/mine', methods = ['GET'])
 def mine():
