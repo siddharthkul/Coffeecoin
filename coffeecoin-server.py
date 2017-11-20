803040311bec9e2382db6c0dc6978f24895d05f2
siddharthkul <siddharthkul1995@gmail.com>
2017-11-20 01:50:39 -0800

    Removed /mine2 method

--- coffeecoin-server.py
+++ coffeecoin-server.py
@@ -142,54 +142,5 @@ def print_blocks():
       blocklist =  blocklist + assembled
   return blocklist
 
-@node.route('/mine2', methods = ['GET'])
-def mine():
-  if(len(this_nodes_transactions) != 0):
-    # Get the last proof of work
-    last_block = blockchain[len(blockchain) - 1]
-    last_proof = last_block.data['proof-of-work']
-    # Find the proof of work for
-    # the current block being mined
-    # Note: The program will hang here until a new
-    #       proof of work is found
-    proof = proof_of_work(last_proof)
-    # Once we find a valid proof of work,
-    # we know we can mine a block so 
-    # we reward the miner by adding a transaction
-    this_nodes_transactions.append(
-      { "from": "network", "to": miner_address, "amount": 1 }
-    )
-    # Now we can gather the data needed
-    # to create the new block
-    new_block_data = {
-      "proof-of-work": proof,
-      "transactions": list(this_nodes_transactions)
-    }
-    new_block_index = last_block.index + 1
-    new_block_timestamp = this_timestamp = date.datetime.now()
-    last_block_hash = last_block.hash
-    # Empty transaction list
-    this_nodes_transactions[:] = []
-    # Now create the
-    # new block!
-    mined_block = Block(
-      new_block_index,
-      new_block_timestamp,
-      new_block_data,
-      last_block_hash
-    )
-    blockchain.append(mined_block)
-
-    # Let the client know we mined a block
-    return json.dumps({
-        "index": new_block_index,
-        "timestamp": str(new_block_timestamp),
-        "data": new_block_data,
-        "hash": last_block_hash
-    }) + "\n"
-  else:
-    print "Spam Miner"
-    return "Sorry\n"
-
 node.run()
 
