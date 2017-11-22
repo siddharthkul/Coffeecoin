d29f2bb1eeec419039ae67027adff25a2290f4e5
siddharthkul <siddharthkul1995@gmail.com>
2017-11-21 17:37:01 -0800

    Added Basic Leaderboard

---
+++ coffeecoin-board.py
@@ -0,0 +1,21 @@
+import time
+import json
+import urllib
+import urllib2
+import os
+
+try:
+    while True:
+        req = urllib2.Request("http://localhost:5000/board", json.dumps({ 'ping_id' : time.time()}), headers={'Content-type': 'application/json', 'Accept': 'application/json'})
+        response = urllib2.urlopen(req)
+        os.system('clear')
+        board = response.read()
+        d = json.loads(board)
+        print "LEADERBOARD"
+        print "-----------"
+        print "Miner# \t Coins Earned"
+        for i in d:
+            print str(d[i]['miner_address']) + "\t\t" + str(d[i]['coins_earned'])
+        time.sleep(1) 
+except KeyboardInterrupt:
+    print('interrupted!')
